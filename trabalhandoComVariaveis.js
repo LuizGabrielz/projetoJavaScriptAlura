f2fe31aeff8a95c036b9accdbb9e27799be01f09
Gabriel <luizgabrielzy@gmail.com>
2022-09-20 09:15:42 -0300

    testando

--- trabalhandoComVariaveis.js
+++ trabalhandoComVariaveis.js
@@ -1,10 +1,1 @@
-//console.log("Trabalhando com variáveis");
-console.log("Trabalhando com variaveis");
-//JS é case sensitive
-
-const idade = 29;
-const nome = "Ricardo";
-
-
-//não fazer 
-//mes = "janeiro";
+console.log('mostrando tudo')
