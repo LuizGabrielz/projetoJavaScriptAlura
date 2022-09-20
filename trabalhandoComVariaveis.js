3e21fd494c27afa2b9de32a93b426d56761ed6ef
Gabriel <luizgabrielzy@gmail.com>
2022-09-20 09:13:03 -0300

    trocando variaveis para teste

---
+++ trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+//console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variaveis");
+//JS é case sensitive
+
+const idade = 29;
+const nome = "Ricardo";
+
+
+//não fazer 
+//mes = "janeiro";
